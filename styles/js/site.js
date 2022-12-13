72775b8cb9a788c7070e9f41adfee32caf339921
DiogoVieira4273 <2220872@my.ipleiria.pt>
2022-12-13 10:12:59 +0000

    first commit

---
+++ styles/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#WEATHER').DataTable();
+});
+function converter_graus()
+{
+     
+}
+
+function favorito() {
+    var img = document.getElementById("star");
+    if (img.style.visibility === "hidden") 
+    {
+        img.style.visibility = "visible";
+    } 
+    else 
+    {
+        img.style.visibility = "hidden";
+    }
+}
